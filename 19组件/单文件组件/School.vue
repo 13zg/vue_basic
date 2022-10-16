c247ca207b4807211c927bc6b98cdd97803bfe8a
zg <115922426+13zg@users.noreply.github.com>
2022-10-16 17:14:30 +0800

    Add files via upload

---
+++ 19组件/单文件组件/School.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class='bg'>
+        <h4>学校名称：{{name}}</h4>
+        <h4>学校地址：{{adress}}</h4>
+    </div>
+</template>
+
+<script>
+// 暴露接口
+    export default {
+        name:'School',
+        data(){
+            return{
+                name:'CUIT',
+                adress:'双流'
+            }
+        }
+}
+</script>
+
+<style>
+.bg{
+    background-color: skyblue;
+}
+</style>
---
+++ 19组件/单文件组件/Student.vue
@@ -0,0 +1,22 @@
+<template>
+   <div>
+        <h4>学生姓名：{{name}}</h4>
+        <h4>学生年龄：{{age}}</h4>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Student',
+    data(){
+            return{
+                name:'Yee',
+                age:18
+            }
+        }
+}
+</script>
+
+<style>
+
+</style>
